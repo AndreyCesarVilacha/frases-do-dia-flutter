ccd51196b08e0690f8e93e9028e3a4c9b33e638b
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-07-23 07:33:17 -0300

    Finalizado o APP frases do dia

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,80 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-//criando a função principal
-void main(){
-  //chamando a função runApp para executar o nosso App
+void main() {
   runApp(MaterialApp(
-    //usando a propriedade "title" do MaterialApp
-    title: "Frases do dia",
-    //Passando o Widget "Column" para a propriedade home
-    home: Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          TextButton(
-            onPressed: (){
-              print("Botão pressionado");
-            }, 
-            child: Text(
-              "Clique Aqui",
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var _frases =[
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+    "Frase 5",
+    "Frase 6",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt(_frases.length);
+    
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.blue[300],
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("images/logo.png"),
+            Text(
+              _fraseGerada,
+              textAlign: TextAlign.justify,
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 17,
+                fontStyle: FontStyle.italic,
                 color: Colors.black,
-                decoration: TextDecoration.none,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _gerarFrase,
+              child: Text(
+                "Nova mensagem",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
               ),
             )
-          )
-        ],
+          ],
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
